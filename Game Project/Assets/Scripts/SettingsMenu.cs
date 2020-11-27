5f0d8a1d8f9d8e37d7abb7942deb1fb2427c99d7
ItsDomHD <itsdomhd@gmailcom>
2020-11-27 09:41:05 +0000

    Shooting Range Asset Imported

--- Game Project/Assets/Scripts/SettingsMenu.cs
+++ Game Project/Assets/Scripts/SettingsMenu.cs
@@ -4,12 +4,15 @@ using UnityEngine.Audio;
 namespace DefaultNamespace
 {
     public class SettingsMenu : MonoBehaviour
-    
+
     {
+        private float currentVolume;
+        
         public AudioMixer audioMixer;
         public void SetVolume(float volume)
         {
             audioMixer.SetFloat("volume", volume);
+            currentVolume = volume;
         }
 
         public void SetQuality(int qualityIndex)
